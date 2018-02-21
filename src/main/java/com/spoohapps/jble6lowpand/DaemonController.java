9ffe6d75fab970f380e5588857b75c932fd1d5f2
Joel Frazier <frazieje@gmail.com>
2018-02-21 01:55:04 -0500

    simple daemon controller

--- src/main/java/com/spoohapps/jble6lowpand/DaemonController.java
+++ src/main/java/com/spoohapps/jble6lowpand/DaemonController.java
@@ -1,4 +1,28 @@
 package com.spoohapps.jble6lowpand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class DaemonController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DaemonController.class);
+
+    public static void main(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            ScanningDaemonController stub = (ScanningDaemonController) registry.lookup("ScanningDaemonController");
+            Set<String> devices = stub.getKnownDevices();
+            logger.info(stub.getKnownDevices().stream().collect(Collectors.joining(", ")));
+        } catch (Exception e) {
+            logger.error("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
 }
