25865740dce1b38dd126bb55d98294cf6be935d4
Joel Frazier <joel.frazier@digitalturbine.com>
2019-01-14 01:16:44 -0500

    define config flags

--- src/main/java/com/spoohapps/jble6lowpand/config/DaemonConfig.java
+++ src/main/java/com/spoohapps/jble6lowpand/config/DaemonConfig.java
@@ -1,9 +1,20 @@
 package com.spoohapps.jble6lowpand.config;
 
+import com.spoohapps.farcommon.config.ConfigFlags;
+
 public interface DaemonConfig {
+    @ConfigFlags({"scanDuration", "d"})
     int getScanDurationMs();
+
+    @ConfigFlags({"scanTimeout", "t"})
     int getScanTimeoutMs();
+
+    @ConfigFlags({"connectTimeout", "c"})
     int getConnectTimeoutMs();
+
+    @ConfigFlags({"controllerPort", "p"})
     int getControllerPort();
+
+    @ConfigFlags({"whitelistPath", "w"})
     String getWhitelistPath();
 }
