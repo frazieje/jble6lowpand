3bbbce039364ac25c60ff77d96cf8f74dcd76708
Joel Frazier <frazieje@gmail.com>
2019-08-24 16:12:44 -0400

    optimize scanning and connection process

--- src/main/java/com/spoohapps/jble6lowpand/manager/DeviceServiceManager.java
+++ src/main/java/com/spoohapps/jble6lowpand/manager/DeviceServiceManager.java
@@ -6,9 +6,12 @@ import com.spoohapps.farcommon.model.EUI48Address;
 import com.spoohapps.jble6lowpand.DeviceService;
 import com.spoohapps.jble6lowpand.model.DeviceServiceStatus;
 import com.spoohapps.jble6lowpand.model.KnownDeviceRepository;
+import org.glassfish.grizzly.utils.ArraySet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -64,30 +67,25 @@ public class DeviceServiceManager extends AbstractManager<DeviceServiceStatus> {
 
         logger.trace("Starting process");
 
-        logger.trace("Scanning for available devices");
-        try {
-            EUI48Address[] devices = deviceService.scanDevices(scanDurationSeconds);
-            Set<EUI48Address> availableAddresses = new HashSet<>();
-            logger.trace("{} available devices found", devices.length);
-            for (EUI48Address device : devices) {
-                try {
-                    logger.trace("adding available device {}", device);
-                    availableAddresses.add(device);
-                } catch (IllegalArgumentException iae) {
-                    logger.trace("Error deserializing EUI48 address");
-                }
-            }
-            availableDevices.retainAll(availableAddresses);
-            availableDevices.addAll(availableAddresses);
-        } catch (Exception e) {
-            logger.error("Error in scan process", e);
-        }
-        logger.trace("Done scanning");
+        Set<EUI48Address> connectedAddresses = updateConnectedDevices();
 
+        disconnectFromUnknownDevices(connectedAddresses);
+
+        Set<EUI48Address> availableAddresses = scanForDevices();
+
+        connectToKnownDevices(availableAddresses, connectedAddresses);
+
+        updateConnectedDevices();
+
+        logger.trace("Process complete");
+
+    }
+
+    private void connectToKnownDevices(Set<EUI48Address> availableAddresses, Set<EUI48Address> connectedAddresses) {
         logger.trace("Connecting to known devices...");
         try {
-            for (EUI48Address address : availableDevices) {
-                if (knownDevices.contains(address) && !connectedDevices.contains(address)) {
+            for (EUI48Address address : availableAddresses) {
+                if (knownDevices.contains(address) && !connectedAddresses.contains(address)) {
                     logger.info("Connecting to {} ... ", address.toString());
                     if (deviceService.connectDevice(address)) {
                         logger.info("Connected to {}", address.toString());
@@ -101,30 +99,65 @@ public class DeviceServiceManager extends AbstractManager<DeviceServiceStatus> {
             logger.error("Error connecting to known devices", e);
         }
         logger.trace("Done connecting to known devices");
+    }
 
-        logger.trace("Updating connected devices");
+    private void disconnectFromUnknownDevices(Set<EUI48Address> connectedAddresses) {
+        logger.trace("Disconnecting from unknown devices...");
         try {
-            Set<EUI48Address> connectedAddresses = new HashSet<>();
-            for (EUI48Address address : deviceService.getConnectedDevices()) {
-                connectedAddresses.add(address);
+            for (EUI48Address address : connectedAddresses) {
                 if (!knownDevices.contains(address)) {
+                    logger.info("Disconnected from {} ... ", address.toString());
                     if (deviceService.disconnectDevice(address)) {
-                        logger.info("Disconnecting from {}", address.toString());
+                        logger.info("Disconnected from {}", address.toString());
                         Thread.sleep(disconnectTimeoutMs);
                     } else {
                         logger.error("Could not disconnect from {}", address.toString());
                     }
                 }
             }
+        } catch (Exception e) {
+            logger.error("Error disconnecting from known devices", e);
+        }
+        logger.trace("Done disconnecting from known devices");
+    }
+
+    private Set<EUI48Address> scanForDevices() {
+        logger.trace("Scanning for available devices");
+        Set<EUI48Address> availableAddresses = new HashSet<>();
+        try {
+            EUI48Address[] devices = deviceService.scanDevices(scanDurationSeconds);
+            logger.trace("{} available devices found", devices.length);
+            for (EUI48Address device : devices) {
+                logger.trace("adding available device {}", device);
+                availableAddresses.add(device);
+            }
+            availableDevices.retainAll(availableAddresses);
+            availableDevices.addAll(availableAddresses);
+        } catch (Exception e) {
+            logger.error("Error in scan process", e);
+        }
+        logger.trace("Done scanning");
+        return availableAddresses;
+    }
+
+    private Set<EUI48Address> updateConnectedDevices() {
+
+        Set<EUI48Address> connectedAddresses = new HashSet<>();
+
+        logger.trace("Updating connected devices");
+        try {
+
+            connectedAddresses.addAll(Arrays.asList(deviceService.getConnectedDevices()));
+
             connectedDevices.retainAll(connectedAddresses);
             connectedDevices.addAll(connectedAddresses);
+
         } catch (Exception e) {
             logger.error("Error updating connected devices", e);
         }
         logger.trace("Done updating connected devices");
 
-        logger.trace("Process complete");
-
+        return connectedAddresses;
     }
 
     @Override
